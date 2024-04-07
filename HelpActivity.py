e0e96f3707cc52d946fdcf0aaa4807cbcef1dd2f
fedorkolotilin <fedorkolotilin3@gmail.com>
2024-04-08 00:31:55 +0300

    added Help text and no library choosed

--- HelpActivity.py
+++ HelpActivity.py
@@ -1,6 +1,9 @@
+import Log
 from Activity import Activity
 
 
 class HelpActivity(Activity):
     def __init__(self, widget, key_listener, parent_activity=None, activity_name=""):
         super().__init__(widget, key_listener, parent_activity, activity_name)
+        self.name_content.text_of_content = open("files/HelpText", 'r').read()
+        # Log.print(self.activity_name)
